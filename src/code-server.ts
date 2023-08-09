c9324f3448d5cb39f78013200e2172884cffcbb7
bbrfkr <bbrfkr@gmail.com>
2023-08-09 11:25:24 +0900

    remove desktop

--- src/code-server.ts
+++ src/code-server.ts
@@ -34,6 +34,14 @@ export class CodeServerStack extends TerraformStack {
       apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
       apt-get -y update && apt-get -y install terraform
 
+      # install mysql-client
+      apt install -y mysql-client
+
+      # install mongosh
+      wget -qO- https://www.mongodb.org/static/pgp/server-6.0.asc | tee /etc/apt/trusted.gpg.d/server-6.0.asc
+      echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+      apt-get update && apt-get install -y mongodb-mongosh
+
       # install code server
       curl -fOL https://github.com/coder/code-server/releases/download/v4.5.0/code-server_4.5.0_amd64.deb
       dpkg -i code-server_4.5.0_amd64.deb
@@ -43,7 +51,7 @@ export class CodeServerStack extends TerraformStack {
       apt-get -y install make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 
       # install nvidia driver
-      apt -y install nvidia-driver-515
+      apt -y install nvidia-driver-535
 
       # install nvidia container driver
       curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | apt-key add -
@@ -51,14 +59,6 @@ export class CodeServerStack extends TerraformStack {
       curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | tee /etc/apt/sources.list.d/nvidia-docker.list
       apt -y update
       apt install -y nvidia-docker2
-
-      # install desktop
-      apt -y install ubuntu-desktop
-      apt -y install xrdp
-      sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
-      wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
-      apt -y update
-      apt install google-chrome-stable
 
       # mount volume
       echo '/dev/vdb /root ext4 defaults 0 0' >> /etc/fstab
@@ -70,7 +70,7 @@ export class CodeServerStack extends TerraformStack {
     new ComputeInstanceV2(this, 'CodeServer', {
       name: 'code-server',
       imageName: 'ubuntu-jammy',
-      flavorName: 'p1.2xlarge',
+      flavorName: 'p1.6xlarge',
       keyPair: 'bbrfkr',
       securityGroups: ['allow-all'],
       network: [{ name: 'common' }],
